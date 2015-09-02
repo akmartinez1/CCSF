bfbeefd5194bee040d20d9115b8a2fa2fe09662c
Alan K. Martinez <akmartinez1@yahoo.com>
2015-09-02 07:03:06 -0700

    CS 151A Homework Assignment Template
    Personalized homework assignment template for CS 151A.  File format A
    (assignment) xx (assignment number from 01 to 16) _Lasnmae. sql for sql
    scrip or let for spool file results.

---
+++ CS 151A/axx_martinez.sql
@@ -0,0 +1,44 @@
+set echo on
+set feedback 1
+set pagesize 999
+set trimspool on
+set linesize 200
+set tab off
+clear columns
+
+
+/* Alan K. Martinez */
+
+/*  TASK 00 */
+select user, sysdate
+from dual;
+
+/*  TASK 01 */
+replace this line with your SQL for task 01
+
+
+/*  TASK 02  */
+
+
+/*  TASK 03 */
+
+
+/*  TASK 04 */
+
+
+/*  TASK 05 */
+
+
+/*  TASK 06 */
+
+
+/*  TASK 07 */
+
+
+/*  TASK 08 */
+
+
+/*  TASK 09 */
+
+
+/*  TASK 10 */
