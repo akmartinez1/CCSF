3314f6948c680d01d07d649196e8f9926b6d0db8
Alan K. Martinez <akmartinez1@yahoo.com>
2016-02-24 21:17:50 -0800

    CS 111B 2016-02-22 Homework
    Edit for correct  output question regarding what is in “name” variable.

--- CS 111B/2016 Spring/Homework/src/hw_2016_02_22_q10.java
+++ CS 111B/2016 Spring/Homework/src/hw_2016_02_22_q10.java
@@ -6,6 +6,6 @@ public class hw_2016_02_22_q10 {
     public static void main (String[] args){
         String name = "Jane Doe";
         String otherName = name.substring(0, 4);
-        System.out.println(otherName);
+        System.out.println(name);
     }
 }
